54a21d7caea2418c24cf4d8a2cba79ccc6174b22
Marek Kroemeke <kroemeke@gmail.com>
2021-12-13 12:44:11 +0000

    Add -r / refresh thread that prints shared map
    Now passing -r doesn't print out map every N lines - instead
    we spawn a new thread and lock map during calculations, sleeping
    in between.
    
    Signed-off-by: Marek Kroemeke <kroemeke@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,57 @@
+#![allow(unused_assignments)]
 use clap::{App, Arg};
 use ctrlc;
 use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
+use std::process;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
 
-fn print_map(map: &HashMap<String, usize>, size: usize) {
+fn print_map(map: Arc<Mutex<HashMap<String, usize>>>, size: usize) {
     let mut count: usize = 0;
-
-    // convert map to vector and sort it by value
+    let xmap = map.lock().unwrap();
+    let map = xmap.clone();
+    drop(xmap);
     let mut hash_vec: Vec<(&String, &usize)> = map.iter().collect();
     // this is cpu intensive
     hash_vec.sort_by(|a, b| b.1.cmp(a.1));
     print!("\x1B[2J\x1B[1;1H");
     for (k, v) in hash_vec {
-        println!("{} {}", k, v);
-        count += 1;
-        if count >= size {
-            break;
-        };
+        {
+            println!("{} {}", k, v);
+            count += 1;
+            if count >= size {
+                count = 0;
+                break;
+            };
+        }
+    }
+}
+
+fn print_map_loop(map: Arc<Mutex<HashMap<String, usize>>>, size: usize, refresh: u64) {
+    let mut count: usize = 0;
+    thread::sleep(time::Duration::from_secs(5));
+    loop {
+        let xmap = map.lock().unwrap();
+        let map = xmap.clone();
+        drop(xmap);
+        let mut hash_vec: Vec<(&String, &usize)> = map.iter().collect();
+        // this is cpu intensive
+        hash_vec.sort_by(|a, b| b.1.cmp(a.1));
+        print!("\x1B[2J\x1B[1;1H");
+        for (k, v) in hash_vec {
+            {
+                println!("{} {}", k, v);
+                count += 1;
+                if count >= size {
+                    count = 0;
+                    break;
+                };
+            }
+        }
+        thread::sleep(time::Duration::from_secs(refresh));
     }
 }
 
@@ -34,64 +66,81 @@ fn main() {
         .long("topn")
         .short("t")
         .takes_value(true)
-        .help("Display top N words")
-        .required(true);
+        .default_value("10")
+        .help("Display top N words");
 
     let refresh = Arg::with_name("refresh")
         .long("refresh")
         .short("r")
         .takes_value(true)
-        .help("Refresh sorted top every N lines")
-        .required(false);
+        .default_value("5")
+        .help("Refresh every <N> seconds.");
 
-    let app = app.args(&[topn, refresh]);
+    let line = Arg::with_name("line")
+        .long("line")
+        .short("l")
+        .takes_value(false)
+        .help("Line mode - count same lines not words.");
+
+    let app = app.args(&[topn, refresh, line]);
     let matches = app.get_matches();
 
     let i = matches
         .value_of("topn")
-        .expect("This can't be none")
+        .expect("topn can't be none")
         .parse::<usize>()
         .unwrap();
 
-    let mut r: usize = 100;
-    if matches.is_present("refresh") {
-        r = matches
-            .value_of("refresh")
-            .expect("Missing refresh?")
-            .parse::<usize>()
-            .unwrap();
-    }
+    let r = matches
+        .value_of("refresh")
+        .expect("refresh can't be none")
+        .parse::<u64>()
+        .unwrap();
 
-    let mut wordmap = HashMap::new();
-    let mut count: usize = 0;
+    let l = matches.is_present("line");
+
+    let wordmap = Arc::new(Mutex::new(HashMap::new()));
 
     // end of argument parsing boilerplate
     //
     ctrlc::set_handler(|| {
-        println!("Final wordmap:");
-        //print_map(&wordmap, i);
+        println!("ctrl-c");
+        process::exit(0);
     })
     .unwrap();
 
+    let arc_map = wordmap.clone();
+
+    // Don't spawn a thread that prints top if we passed -r 0
+    if r != 0 {
+        thread::spawn(move || print_map_loop(arc_map, i, r));
+    }
+
+    let wordmap = wordmap.clone();
     let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        count += 1;
-        match line {
-            Err(_) => {
-                print_map(&wordmap, 1);
-                break;
-            }
-            Ok(s) => {
-                for word in s.split_whitespace() {
-                    *wordmap.entry(word.to_string()).or_insert(0) += 1;
+    if l == false {
+        for line in stdin.lock().lines() {
+            match line {
+                Err(_) => {}
+                Ok(s) => {
+                    for word in s.split_whitespace() {
+                        let mut map = wordmap.lock().unwrap();
+                        *map.entry(word.to_string()).or_insert(0) += 1;
+                    }
                 }
             }
         }
-        if count % r == 0 {
-            print_map(&wordmap, i);
+    } else {
+        for line in stdin.lock().lines() {
+            match line {
+                Err(_) => {}
+                Ok(s) => {
+                    let mut map = wordmap.lock().unwrap();
+                    *map.entry(s.to_string()).or_insert(0) += 1;
+                }
+            }
         }
     }
-    // TODO: remove sleep for 1 second before final update and exit - used for pv benchmark
-    thread::sleep(time::Duration::from_secs(1));
-    print_map(&wordmap, i);
+    let arc_map = wordmap.clone();
+    print_map(arc_map, i);
 }
