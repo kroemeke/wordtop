a07f9ef42b6935383141bc26e36c93db9db82237
Marek Kroemeke <kroemeke@gmail.com>
2021-12-27 16:33:39 +0000

    Split into modules
    Split into modules.
    Work in progress for display sorted by rate not total count.
    Work in progress on some refactoring print related bits
    
    Signed-off-by: Marek Kroemeke <kroemeke@gmail.com>

---
+++ src/calculate.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+
+/// Provided with two hashmaps where value is usize,
+/// calculate rate per second.
+// TODO change key/value into a tuple
+// TODO change return to Option
+#[inline]
+pub fn calculate_rate(
+    map: &HashMap<String, usize>,
+    key: &str,
+    value: &usize,
+    seconds: u64,
+) -> Option<usize> {
+    let s = seconds as usize;
+    match map.get(key) {
+        Some(&p) => {
+            let rate = (value - p) / s;
+            Some(rate)
+        }
+        // No key in map, so we return Option->None
+        _ => None,
+    }
+}
+
+/// given two <String, usize> maps and time delta, return
+/// a <String, usize> map where value is rate per second
+pub fn calculate_rate_top(
+    old_map: &HashMap<String, usize>,
+    new_map: &HashMap<String, usize>,
+    seconds: usize,
+    items: usize,
+) -> HashMap<String, usize> {
+    let mut result = HashMap::new();
+    // iterate over new_map
+    // for each item in the new map, check if we have an entry in old map
+    // if yes - insert into result entry with (new_map[k] - old_map[k]) / seconds
+    // if no - insert into result entry with new_map[k] / seconds
+    // Once done for all elements - convert to vector, sort by value
+    // and return top N results as either a map or vec
+    for (k, v) in new_map.iter() {
+        let rate = (v - old_map.get(k).unwrap_or(&0)) / seconds;
+        result.insert(k.to_string(), rate);
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_calculate_rate() {
+        let mut test_map = HashMap::new();
+        test_map.insert("dupa".to_string(), 20);
+        let rate = calculate_rate(&test_map, &"dupa".to_string(), &300, 5);
+        assert_eq!(rate.unwrap(), 56);
+    }
+
+    #[test]
+    fn test_calculate_rate_top() {
+        let mut old_map = HashMap::new();
+        let mut new_map = HashMap::new();
+        old_map.insert("dupa".to_string(), 10);
+        new_map.insert("dupa".to_string(), 110);
+        let result = calculate_rate_top(&old_map, &new_map, 10, 15);
+        for (k, v) in result.iter() {
+            println!("result k:{} v:{}", k, v);
+        }
+        assert_eq!(result.get(&"dupa".to_string()).unwrap(), &10);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,97 +6,9 @@ use std::io::BufRead;
 use std::process;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time;
 
-/// Provided with two hashmaps where value is usize,
-/// calculate rate per second.
-// TODO change key/value into a tuple
-// TODO change return to Option
-fn calculate_rate(
-    map: &HashMap<String, usize>,
-    key: &String,
-    value: &usize,
-    seconds: u64,
-) -> Option<usize> {
-    let s = seconds as usize;
-    match map.get(key) {
-        Some(&p) => {
-            let rate = (value - p) / s;
-            Some(rate)
-        }
-        // No key in map, so we return Option->None
-        _ => None,
-    }
-}
-
-fn print_map(map: Arc<Mutex<HashMap<String, usize>>>, size: usize) {
-    let mut count: usize = 0;
-    let xmap = map.lock().unwrap();
-    let map = xmap.clone();
-    drop(xmap);
-    let mut hash_vec: Vec<(&String, &usize)> = map.iter().collect();
-    // this is cpu intensive
-    hash_vec.sort_by(|a, b| b.1.cmp(a.1));
-    print!("\x1B[2J\x1B[1;1H");
-    for (k, v) in hash_vec {
-        {
-            println!("{: <20} {}", k, v);
-            count += 1;
-            if count >= size {
-                count = 0;
-                break;
-            };
-        }
-    }
-}
-
-// TODO add sorting by rate
-// TODO dynamically adjust format {: <20} if keys are too long
-// TODO maybe add tui-rs option for prettier printing
-fn print_map_loop(map: Arc<Mutex<HashMap<String, usize>>>, size: usize, refresh: u64) {
-    let mut count: usize = 0;
-    let mut old_map = HashMap::new();
-    let mut kwidth = 10; // key (word/line) width fill
-    let mut twidth = 10; // value count width
-    loop {
-        let xmap = map.lock().unwrap();
-        let map = xmap.clone();
-        drop(xmap);
-        let mut hash_vec: Vec<(&String, &usize)> = map.iter().collect();
-        // this is cpu intensive
-        hash_vec.sort_by(|a, b| b.1.cmp(a.1));
-        print!("\x1B[2J\x1B[1;1H");
-        for (k, v) in hash_vec {
-            {
-                if let Some(rate) = calculate_rate(&old_map, k, v, refresh) {
-                    if k.len() > kwidth {
-                        kwidth = k.len();
-                    }
-                    if rate.to_string().len() + 3 > twidth {
-                        twidth = rate.to_string().len() + 4;
-                    }
-                    println!(
-                        "{: <kwidth$} {: <twidth$} {}",
-                        k,
-                        format!("[{}/s]", rate),
-                        v,
-                        kwidth = kwidth,
-                        twidth = twidth,
-                    );
-                } else {
-                    println!("{: <kwidth$}  {}", k, v, kwidth = kwidth + 10);
-                }
-                count += 1;
-                if count >= size {
-                    count = 0;
-                    break;
-                };
-            }
-        }
-        old_map = map.clone();
-        thread::sleep(time::Duration::from_secs(refresh));
-    }
-}
+mod calculate; // functions related to rate calculation
+mod print; // functions related to presenting results
 
 fn main() {
     // argument parsing boilerplate
@@ -125,7 +37,16 @@ fn main() {
         .takes_value(false)
         .help("Line mode - count same lines not words.");
 
-    let app = app.args(&[top, refresh, line]);
+    let sort = Arg::with_name("sort")
+        .long("sort")
+        .short("s")
+        .takes_value(true)
+        .possible_values(&["count", "rate"])
+        .hide_possible_values(false)
+        .default_value("count")
+        .help("Sort by");
+
+    let app = app.args(&[top, refresh, line, sort]);
     let matches = app.get_matches();
 
     let t = matches
@@ -142,6 +63,12 @@ fn main() {
 
     let l = matches.is_present("line");
 
+    let s = matches
+        .value_of("sort")
+        .expect("sort can't be none")
+        .parse::<String>()
+        .unwrap();
+
     let wordmap = Arc::new(Mutex::new(HashMap::new()));
 
     // end of argument parsing boilerplate
@@ -156,7 +83,7 @@ fn main() {
 
     // Don't spawn a thread that prints top if we passed -r 0
     if r != 0 {
-        thread::spawn(move || print_map_loop(arc_map, t, r));
+        thread::spawn(move || print::print_map_loop(arc_map, t, r, s));
     }
 
     let wordmap = wordmap;
@@ -184,19 +111,5 @@ fn main() {
             }
         }
     }
-    print_map(wordmap, t);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::collections::HashMap;
-
-    #[test]
-    fn test_calculate_rate() {
-        let mut test_map = HashMap::new();
-        test_map.insert("dupa".to_string(), 20);
-        let rate = calculate_rate(&test_map, &"dupa".to_string(), &300, 5);
-        assert_eq!(rate.unwrap(), 56);
-    }
+    print::print_map(wordmap, t);
 }
